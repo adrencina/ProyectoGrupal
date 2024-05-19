fbddab2309d0fedd7b09fad6d261ad6fc8a1b3b7
claudiocastilloher <107433152+claudiocastilloher@users.noreply.github.com>
2024-05-18 22:42:20 -0600

    Se agregaron las listas correspondiente a integrantes, nosotros, searchfriend.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
